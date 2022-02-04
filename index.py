d728ef3d78f46aa26cc009ffdd553475bdbff183
Ricardo Miliotte <62629414+Miliotte@users.noreply.github.com>
2022-02-04 08:37:31 -0300

    Login

--- index.py
+++ index.py
@@ -6,17 +6,24 @@ from turtle import left
 
 # Criar Windows
 
+
+
 win = Tk()
 win.title("Miliotte System - Acess Panel")
 win.geometry("600x300")
 win.configure(background="white")
 win.resizable(width=FALSE, height=False)
 
+logo = PhotoImage(file="img/logo.png")
+
 LeftFrame = Frame(win, width=200, height=300, bg="MIDNIGHTBLUE", relief="raise")
 LeftFrame.pack(side=LEFT)
 
 Rightrame = Frame(win, width=395, height=300, bg="MIDNIGHTBLUE", relief="raise")
 Rightrame.pack(side=RIGHT)
+
+LogoLabel = Label(LeftFrame, image=logo, bg='MIDNIGHTBLUE')
+LogoLabel.place(x=50 , y=100)
 
 win.mainloop()
 
