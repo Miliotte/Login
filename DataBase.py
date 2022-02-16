ef47830343d8bfa4ea158138b58ab0b268b1b12e
Ricardo Miliotte <62629414+Miliotte@users.noreply.github.com>
2022-02-16 07:27:56 -0300

    Add DataBase

--- DataBase.py
+++ DataBase.py
@@ -1,5 +1,4 @@
 import sqlite3
-from venv import create
 
 conm = sqlite3.connect('Users.db')
 
@@ -7,6 +6,12 @@ cursor = conm.cursor()
 
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS Users (
-    
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    user TEXT NOT NULL,
+    password TEXT NOT NULL
 );
 """)
+
+print("Connected to Database")
